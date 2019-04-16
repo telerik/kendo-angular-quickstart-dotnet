50b5b5c3ca2db4160c2bb58df3462e67ffa0e0f7
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2019-04-16 12:20:10 +0300

    fix: add animations module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -29,6 +30,7 @@ import { GridModule } from '@progress/kendo-angular-grid';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
+    BrowserAnimationsModule,
     GridModule
   ],
   providers: [],
